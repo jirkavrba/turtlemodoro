99b5a4800672f78f959f7aca50e60aada1742606
Jiří Vrba <jiri@vrba.dev>
2020-11-23 01:01:28 +0100

    Added configuration design

--- src/components/Configuration.vue
+++ src/components/Configuration.vue
@@ -1,29 +1,71 @@
 <template>
-    <div class="configuration">
-        <h1 class="timer__phase">Configuration</h1>
+  <div class="configuration">
+    <h1 class="timer__phase">Configuration</h1>
 
-        <label>Pomodoro time: <b>{{ configuration.pomodoro.toString().padStart(2, '0') }}:00</b></label><br>
-        <input type="range" v-model="configuration.pomodoro" min="1" max="60" step="1">
-
-        <hr>
-
-        <label>Short break time: <b>{{ configuration.shortBreak.toString().padStart(2, '0') }}:00</b></label><br>
-        <input type="range" v-model="configuration.shortBreak" min="1" max="60" step="1">
-
-        <hr>
-
-        <label>Long break time: <b>{{ configuration.longBreak.toString().padStart(2, '0') }}:00</b></label><br>
-        <input type="range" v-model="configuration.longBreak" min="1" max="60" step="1">
-
-        <hr>
-
-        <label>Long break intervals: <b>{{ configuration.longBreakIntervals }}</b></label><br>
-        <input type="range" v-model="configuration.longBreakIntervals" min="2" max="10" step="1">
-
-        <hr>
-
-        <button @click="this.start">START</button>
+    <div class="configuration__slider">
+      <label
+        >Pomodoro time:
+        <b
+          >{{ configuration.pomodoro.toString().padStart(2, "0") }}:00</b
+        ></label
+      ><br />
+      <input
+        type="range"
+        v-model="configuration.pomodoro"
+        min="1"
+        max="60"
+        step="1"
+      />
     </div>
+
+    <div class="configuration__slider">
+      <label
+        >Short break time:
+        <b
+          >{{ configuration.shortBreak.toString().padStart(2, "0") }}:00</b
+        ></label
+      ><br />
+      <input
+        type="range"
+        v-model="configuration.shortBreak"
+        min="1"
+        max="60"
+        step="1"
+      />
+    </div>
+
+    <div class="configuration__slider">
+      <label
+        >Long break time:
+        <b
+          >{{ configuration.longBreak.toString().padStart(2, "0") }}:00</b
+        ></label
+      ><br />
+      <input
+        type="range"
+        v-model="configuration.longBreak"
+        min="1"
+        max="60"
+        step="1"
+      />
+    </div>
+
+    <div class="configuration__slider">
+      <label
+        >Long break intervals:
+        <b>{{ configuration.longBreakIntervals }}</b></label
+      ><br />
+      <input
+        type="range"
+        v-model="configuration.longBreakIntervals"
+        min="2"
+        max="10"
+        step="1"
+      />
+    </div>
+
+    <button @click="this.start">START</button>
+  </div>
 </template>
 <script>
 export default {
