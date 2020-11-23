62a4e75af96041c02cde84a1285018b7dbe16d0a
Jiří Vrba <jiri@vrba.dev>
2020-11-23 10:14:29 +0100

    Added toggle to control enlarging the stream during pauses

--- src/components/Stream.vue
+++ src/components/Stream.vue
@@ -1,17 +1,33 @@
 <template>
-    <img :src="streamUrl" width="640" height="480" :class="'stream ' + (this.phase == 'pomodoro' ? 'stream--minified' : '')">
+    <div>
+        <img :src="streamUrl" width="640" height="480" :class="'stream ' + this.streamClass">
+        <div class="stream__control">
+            <input id="enlarge_stream" v-model="enlargeDuringPauses" type="checkbox">
+            <label for="enlarge_stream">Enlarge stream during pauses</label>
+        </div>
+    </div>
 </template>
 <script>
 export default {
     name: "Stream",
     props: ["phase"],
     data: () => ({
-        streamUrl: ""
+        streamUrl: "",
+        enlargeDuringPauses: false,
     }),
     methods: {
         synchronizeImage: function () {
             // This is just a nasty trick to prevent self assign errors
             this.streamUrl = this.streamUrl.replace("", "");
+        },
+    },
+    computed: {
+        streamClass() {
+            if (this.phase != "pomodoro" && this.enlargeDuringPauses) {
+                return 'stream--enlarged';
+            }
+
+            return '';
         }
     },
     mounted: async function () {
